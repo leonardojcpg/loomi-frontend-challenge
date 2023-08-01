7e1ac8bca69fbdbcc056a51870910193f9eb959e
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-01 13:48:35 -0300

    feat: Creating userForm component

---
+++ src/Components/UserForm/index.jsx
@@ -0,0 +1,11 @@
+export const UserForm = () => {
+    return (
+        <div className="userFormContainer">
+            <div className="leafImg"></div>
+            <div className="helloMessageParagraph"></div>
+            <div className="form"></div>
+            <div className="forgotPassword"></div>
+            <div className="button"></div>
+        </div>
+    )
+}
