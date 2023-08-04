68c5618b4a9601c677002478f5ba041706c5658e
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-04 14:08:10 -0300

    feat: Importing yup library and making validation

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,16 +1,60 @@
+/* eslint-disable react/no-unknown-property */
 import PropTypes from "prop-types";
-
 import { CheckboxContainer, CheckboxLabel } from "./styles.js";
 
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {useHistory} from 'react-router-dom'
+
 export const Form = ({ labelText, labelPasswordText, checkboxText }) => {
+  const schema = yup.object().shape({
+    username: yup.string().required("Usuário obrigatorio"),
+    password: yup.string().required("Senha Obrigatoria"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const history = useHistory();
+
+  const handleSignup = (data) => {
+    api
+      .post("/users", data)
+      .then((response) => {
+        toast.success("Usuário cadastrado com sucesso!");
+        history.push("/");
+      })
+      .catch((err) => {
+        toast.error("email ja está em uso");
+      });
+  };
+
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>{labelText}</label>
-        <input className="inputName" type="name" />
+        <input
+          className="inputName"
+          type="name"
+          {...register("user")}
+          error={!!errors.user?.message}
+        />
 
         <label>{labelPasswordText}</label>
-        <input className="inputPassword" type="password" />
+        <input
+          className="inputPassword"
+          type="password"
+          {...register("password")}
+          error={!!errors.password?.message}
+        />
 
         <CheckboxContainer>
           <input
