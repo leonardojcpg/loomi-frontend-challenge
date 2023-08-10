2d2c3d279116759bdaa5fb78fe356f8c835f386a
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-10 19:14:40 -0300

    refactor: Refactoring loginForm and using Yup form validation

--- src/Components/Form/LoginForm/loginForm.jsx
+++ src/Components/Form/LoginForm/loginForm.jsx
@@ -1,76 +1,57 @@
 /* eslint-disable react/no-unknown-property */
 import PropTypes from "prop-types";
 import { ForgotPassword } from "./loginFormStyles";
-import { useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { Button } from "../../Button/index.jsx";
-import { toast } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
+import { Api } from "../../../Service/index.jsx";
+import { toast } from "react-toastify";
 
 export const LoginForm = ({ labelText, labelPasswordText }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const schema = yup.object({
+    email: yup.string().required("Email required"),
+    password: yup.string().required("Password required"),
+  });
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleLogin = async (data) => {
     try {
-      const response = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
-
-      if (response.status === 200) {
-        const data = await response.json();
-        const token = data.token;
-        localStorage.setItem("authToken", token);
-        try {
-          const authenticatedResponse = await fetch(
-            "http://localhost:3000/users",
-            {
-              method: "GET",
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-
-          if (authenticatedResponse.status === 200) {
-            toast.success("Login successful");
-          } else {
-            toast.error("Failed to fetch dashboard data");
-          }
-        } catch (error) {
-          toast.error(`Error fetching dashboard data: ${error.message}`);
-        }
-      } else {
-        toast.error("Login failed");
-      }
-    } catch (error) {
-      toast.error(`Error logging in: ${error.message}`);
+      const response = await Api.post("/login", data);
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      toast.success("Logged in successfully!");
+    } catch (errror) {
+      toast.error("Error loggin in:", errors);
     }
   };
 
   return (
-    <form onSubmit={handleLogin}>
+    <form onSubmit={handleSubmit(handleLogin)}>
       <label>{labelText}</label>
       <input
         className="inputName"
         type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        {...register("email")}
+        {...errors.email?.message}
       />
 
       <label>{labelPasswordText}</label>
       <input
         className="inputPassword"
         type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        {...register("password")}
+        {...errors.password?.message}
       />
       <ForgotPassword>
         <a href="_blank">Esqueci minha senha</a>
